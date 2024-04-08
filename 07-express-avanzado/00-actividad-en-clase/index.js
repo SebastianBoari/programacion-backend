e5d4b5838d52eca08e562884bb3e44661b6980ef
sebastian <luciosebastianboari@gmail.com>
2024-04-07 21:31:58 -0300

    clase-07

--- 07-express-avanzado/00-actividad-en-clase/index.js
+++ 07-express-avanzado/00-actividad-en-clase/index.js
@@ -37,12 +37,38 @@ app.post('/api/palabras', (req, res) => {
 })
 
 app.put('/api/palabras/:pos', (req, res) => {
+    let position = Number(req.params.pos)
 
+    if (position <= 0) return res.status(400).json({ error: "Por favor, ingrese un valor mayor a 0." })
 
+    const { palabra } = req.body
+
+    if (!palabra || palabra.length <= 0) return res.status(400).json({ error: "Por favor, ingrese una palabra." })
+
+    const splittedQuote = quote.split(' ')
+
+    const wordToReplace = splittedQuote[position - 1]
+    const indexOfWordToReplace = splittedQuote.indexOf(wordToReplace)
+
+    splittedQuote.splice(indexOfWordToReplace, 1, palabra)
+
+    res.status(200).json({ actualizada: palabra, anterior: wordToReplace })
 })
 
 app.delete('/api/palabras/:pos', (req, res) => {
+    let position = Number(req.params.pos)
 
+    if (position <= 0) return res.status(400).json({ error: "Por favor, ingrese un valor mayor a 0." })
+
+    const splittedQuote = quote.split(' ')
+
+    splittedQuote.splice(position - 1, 1)
+
+    const joinedQuote = splittedQuote.join(' ');
+
+    const formattedQuote = joinedQuote.charAt(0).toUpperCase() + joinedQuote.slice(1).toLowerCase();
+
+    res.status(200).json({ frase: formattedQuote })
 })
 
 
